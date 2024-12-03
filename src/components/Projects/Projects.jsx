029621890fdea8a09c87b6de2ea53ec5c30cfe2c
rodriguessavio <capar.2022113tads0049@aluno.ifpi.edu.br>
2024-12-03 14:22:03 -0300

    alterando css projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
+import React from 'react';
+import styles from '../Projects/Projects.module.css';
+import html from '../../assets/projects/html.svg';
+import arrow from '../../assets/icons/arrow.svg';
 
 function Projects() {
   return (
-    <div>
-        <h1>Projetos</h1>
-        <div>
-            <div>
-                <img src="" alt="" />
-                <a href="">Clique aqui para visitar</a>
+    <div className={styles.projects}>
+        <h1 className={styles.title}>PROJETOS</h1>
+        <div className={styles.content}>
+            <div className={styles.card}>
+                <img src={html} alt="" />
+                <a href="" className={styles.link_project}>
+                  <p>Clique aqui para visitar</p>
+                  <p>HTML TUTORIAL</p>
+                  <img src={arrow} alt="" />
+                </a>
             </div>
 
-            <div>
-                <img src="" alt="" />
-                <a href="">Clique aqui para visitar</a>
+            <div className={styles.card}>
+                <img src={html} alt="" />
+                <a href="" className={styles.link_project}>
+                  <p>Clique aqui para visitar</p>
+                  <p>HTML TUTORIAL</p>
+                  <img src={arrow} alt="" />
+                </a>
             </div>
         </div>
     </div>
