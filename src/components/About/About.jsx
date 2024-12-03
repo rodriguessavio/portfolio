b248cdf0441ea7e9d88b3aa540e329ddcf7505a4
rodriguessavio <capar.2022113tads0049@aluno.ifpi.edu.br>
2024-12-02 23:33:09 -0300

    alterações de estilo em 'about'

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,12 +7,12 @@ function About() {
     <div className={styles.about}>
         <img src={profile} alt="" className={styles.profileimg}/>
         <p className={styles.title}>Transformando ideias em <span>código!</span></p>
-        <p>
+        <p className={styles.about_content}>
             Me chamo Sávio Rodrigues, sou aspirante a desenvolvedor backend. 
             Atualmente direciono meus estudos ao desenvolvimento web back-end com laravel, desenvolvendo aplicações performáticas e fluídas. Além de permanecer aprimorando meus conhecimentos 
             em git, html, css, php e js.
         </p>
-        <a href="">Download CV</a>
+        <a href="" className={styles.downloadcv}>Download CV</a>
 
         <div>
             <h2>EXPERIÊNCIA COM</h2>
